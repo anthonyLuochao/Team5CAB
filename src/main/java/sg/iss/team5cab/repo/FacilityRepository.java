46bab25e6cfb4dbbd093ff7314b0f55d7f7c384e
Divyashree <divyashree.ga@gmail.com>
2018-06-11 20:26:58 +0800

    facility module

--- src/main/java/sg/iss/team5cab/repo/FacilityRepository.java
+++ src/main/java/sg/iss/team5cab/repo/FacilityRepository.java
@@ -1,9 +1,30 @@
 package sg.iss.team5cab.repo;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sg.iss.team5cab.model.Facility;
 
 public interface FacilityRepository extends JpaRepository<Facility, Integer> {
-
+	
+	@Query("SELECT f FROM Facility f where f.facilityname = :name")
+	Facility findByName(@Param("name") String name);
+	
+	@Query("SELECT f FROM Facility f where f.typeID = : type")
+	ArrayList<Facility> findByType(@Param("type") String type);
+	
+	@Query("SELECT f FROM Facility f where f.isDamaged = 1")
+	ArrayList<Facility> findIsDamaged(int isDamaged);
+	
+	@Query("SELECT f FROM Facility f where f.isDeleted = 0")
+	ArrayList<Facility> findIsDeleted(int isDeleted);
+	
+	@Query("SELECT f FROM Facility f Inner Join Booking b on f.facilityID=b.facilityID where f.typeID=:type and f.isDamaged=0 and b.start_Date =: startDate and b.end_Date=: endDate")
+    ArrayList<Facility>findIsAvailable(String typeId,Date startDate,Date endDate,int isDamaged);
+	
+	
 }
--- src/main/java/sg/iss/team5cab/services/FacilityServicesImpl.java
+++ src/main/java/sg/iss/team5cab/services/FacilityServicesImpl.java
@@ -1,18 +1,129 @@
 package sg.iss.team5cab.services;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import sg.iss.team5cab.model.Facility;
 import sg.iss.team5cab.repo.FacilityRepository;
 import sg.iss.team5cab.repo.UsersRepository;
 
 @Service
-public class FacilityServicesImpl {
+public class FacilityServicesImpl implements FacilityServices{
 	
 	@Resource
-	FacilityRepository fRepo;
+	private FacilityRepository fRepo;
+	//private BookingService bService;
 	
-	//@Transactional DAO methods that uses JPARepo
+	@Override
+	@Transactional
+	
+	public ArrayList<Facility> findAllFacilities(){
+		//System.out.println("yes");
+		ArrayList<Facility> f= (ArrayList<Facility>)fRepo.findAll();
+		return f;
+	}
+	
+	@Override
+	@Transactional
+	public Facility findFacility(int fid)
+	{
+		return fRepo.findOne(fid);
+	}
+	
+	@Override
+	@Transactional
+	
+	public ArrayList<Facility> findByTypeList(String typeId){		
+		ArrayList<Facility> f= (ArrayList<Facility>)fRepo.findByType(typeId);		
+		return f;
+	}
+	
+	@Override
+	@Transactional
+	
+	public ArrayList<Facility> findIsDamagedList(int isDamaged){		
+		ArrayList<Facility> f= (ArrayList<Facility>)fRepo.findIsDamaged(isDamaged);		
+		return f;
+	}
 
-}
+//	@Override
+//	@Transactional
+//	public Facility findFacilityName(String fname)
+//	{
+//		return fRepo.findByName(fname);
+//	}
+		
+	@Override
+	@Transactional
+	public Facility createFacility(Facility fac)
+	{
+		return fRepo.saveAndFlush(fac);
+	}
+	
+	@Override
+	@Transactional
+	public Facility updateFacility(Facility fac)
+	{
+		return fRepo.saveAndFlush(fac);
+	}
+	
+//	@Override
+//	@Transactional
+//	public void deleteFacility(Facility fac)
+//	{
+//		fRepo.delete(fac);
+//	}
+	
+	@Override
+	@Transactional
+    public boolean deleteFacility(Facility fac)
+	{
+		//fac.setisDeleted(true);
+		if(fRepo.saveAndFlush(fac).equals(null))
+		{
+			return false;
+		}
+		else
+			return true;		
+		
+	}
+	
+//	@Override
+//	@Transactional
+//	public ArrayList<Facility> findFacility(String typeId,Date startDate,Date endDate,int isDamaged){	
+//		findByTypeList(String typeId);
+//		
+//		
+//		ArrayList<Facility> f= (ArrayList<Facility>)fRepo.findIsAvailable(typeId,startDate,endDate,isDamaged);		
+//		return f;
+//	}
+	
+	
+	
+	@Override
+	@Transactional
+	public ArrayList<Facility> findFacilityByDateRange(LocalDate startDate,LocalDate endDate)
+	{
+		ArrayList<Facility> returnList=new ArrayList<Facility>();
+		ArrayList<Facility> f= (ArrayList<Facility>)fRepo.findAll();
+		for(Facility fac : f)
+		{
+//			boolean result = bService.checkFacilityAvailability(fac.getFacilityID(),startDate,endDate);
+//			if(result)
+//			{
+//				returnList.add(fac);
+//			}
+		}
+		return returnList;
+		
+	}
+	
+}	
+
+
